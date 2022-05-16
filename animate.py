6cdc7b8c8d413ff61c8948a3c3eb35e2656038e2
John Omotani <john.omotani@ukaea.uk>
2022-05-16 17:46:50 +0100

    Don't plot x-boundary points in animation
    Makes colour scale for phi better.

--- animate.py
+++ animate.py
@@ -2,7 +2,7 @@
 
 from xbout import open_boutdataset
 
-ds = open_boutdataset("example-run/BOUT.dmp.*.nc", keep_xboundaries=True).squeeze()
+ds = open_boutdataset("example-run/BOUT.dmp.*.nc", keep_xboundaries=False).squeeze()
 
 #ds.bout.animate_list(["Ne", "Pe", "Pi", "phi", "Vort"], show=True)
 ds.bout.animate_list(["Ne", "Pe", "Pi", "phi", "Vort"], save_as="animation")
